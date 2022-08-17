4c7d603503eaeeeb03a52b5bb3f002149bef6a16
GDenisC <92527725+GDenisC@users.noreply.github.com>
2022-08-17 12:33:25 +0700

    small fix

--- threq.py
+++ threq.py
@@ -15,7 +15,7 @@ def _CreateThread(target: Callable[..., object], *args, **kwargs) -> None:
 
 def ThreadRequest(url: str, callback: Callable[[requests.Response], None] = None, *, method: MethodType = MethodType.GET, args: Dict[str, Any] = {}, **kwargs: Dict[str, Any]) -> None:
     def wrapper(f: Callable[[requests.Response], None] = None):
-        if f == None:
+        if not f:
             _CreateThread(target=lambda: callback(method(url, args, **kwargs)))
             return
         _CreateThread(target=lambda: f(method(url, args, **kwargs)))
